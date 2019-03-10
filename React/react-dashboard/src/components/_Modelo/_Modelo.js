60c49f74a45a4b52da728cb22bf64f4a72399bdb
acgomes68 <acgomes68@gmail.com>
2019-03-10 06:27:49 -0300

    Componentes Menu Superior, Resumo e Modelo

---
+++ React/react-dashboard/src/components/_Modelo/_Modelo.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class _Modelo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+export default _Modelo;
